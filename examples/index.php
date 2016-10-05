ae43c49f779ddeca3542c3469401733d64b3d410
kocsis.mate <kocsismate@woohoolabs.com>
2016-10-06 00:23:31 +0200

    Improved development environment

--- examples/index.php
+++ examples/index.php
@@ -2,10 +2,16 @@
 
 use WoohooLabs\Worm\Examples\Model\UserModel;
 use WoohooLabs\Worm\Connection\PdoConnection;
+use WoohooLabs\Worm\Query\ConditionBuilder;
 use WoohooLabs\Worm\Worm;
 
-$worm = new Worm(PdoConnection::create("", "", "", "", "", ""));
+$worm = new Worm(PdoConnection::create("mysql", "localhost", "", "", "", ""));
 $worm
     ->query(new UserModel())
     ->where("name", "=", "John", "and")
+    ->whereNested(
+        function (ConditionBuilder $condition) {
+            $condition->add("name", "=", "John", "and");
+        }
+    )
     ->getList();
