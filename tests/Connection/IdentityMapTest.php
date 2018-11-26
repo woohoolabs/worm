8612c15fe013c62c1d85cbecb0d8f46b1aafcf11
kocsismate <kocsismate@woohoolabs.com>
2018-11-26 19:09:42 +0100

    Improve local development

---
+++ tests/Connection/IdentityMapTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace WoohooLabs\Worm\Tests\Connection;
+
+use PHPUnit\Framework\TestCase;
+
+class IdentityMapTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function hasObject()
+    {
+        $this->assertTrue(true);
+    }
+}
